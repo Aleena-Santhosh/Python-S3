45763a3dcc104823d52bdd4582a61279bf97393c
Aleena-Santhosh <aleenasanthosh949@gmail.com>
2025-08-18 16:28:21 +0530

    equitri.py

--- equitri.py
+++ equitri.py
@@ -8,7 +8,7 @@ my_turtle = turtle.Turtle()
 my_turtle.pencolor("black") 
 my_turtle.pensize(10) 
 my_turtle.color("black")
-my_turtle.fillcolor("violet")
+my_turtle.fillcolor("white")
 my_turtle.begin_fill()
 
 
@@ -20,7 +20,7 @@ my_turtle.end_fill()
 my_turtle.penup()
 my_turtle.backward(250)
 my_turtle.pendown()
-my_turtle.fillcolor("dark blue")
+my_turtle.fillcolor("white")
 my_turtle.begin_fill()
 
 for _ in range(3):
@@ -31,12 +31,13 @@ my_turtle.end_fill()
 my_turtle.penup()
 my_turtle.backward(150)
 my_turtle.pendown()
-my_turtle.fillcolor("blue")
+my_turtle.fillcolor("white")
 my_turtle.begin_fill()
 
 for _ in range(3):
     my_turtle.forward(100)
     my_turtle.left(120)
 my_turtle.end_fill()
+
 
 turtle.done()
